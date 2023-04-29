8c670f891dcf60e5abeb96169ed5c801e68fce0e
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-04-29 09:24:32 +0530

    initial commit

--- components/About.jsx
+++ components/About.jsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 import css from '../styles/About.module.css';
 import AboutImage from '../assets/about.jpg';
 import { useRouter } from "next/router";
-import Layout from './Layout';
 
 export default function About() {
     const router = useRouter();
+    const s1="Handcrafted Haven offers access to reserves of handcrafted artefacts - either richly curated by partnering with empanelled artisan communities and independent artists. Borrowing inspiration from our \"Rajasthani Karigar\" roots, we devote this gateway to the uniformly evolving and ever-inspirational creation of Indian Artisanship."
     return (
              <div className={css.container} id="about">
                 <div className={css.content}>
                 <h1>About</h1>
             <p>
-            hii
+            {s1}
+{/* <br/>Our mission is to offer the global audience a gateway to hand-picked Indian artefacts and incite a conversation dedicated to conserving arts and crafts. Rajasthan is famously known for the "Treasure trove of Indian handicrafts". These handicrafts have also earned the country endless love. Hence, we bring your handiworks directly here from Rajasthan under one roof.
+<br/>To bring your home to life, you need decor with outstanding pieces of craftsmanship and art that you can see and feel. Every detail matters – clean designs, meticulous craftsmanship, stunning portraits and artefacts, personal touches and colours, and the highest quality materials. The art is functional, beautiful and has a view. You may never know who created these works of art. However, you know where to order - Handcrafted Haven. */}
             </p>
             </div>
             <div className={css.img}>
